d4ff7b5f38cfc4cbc794eb786a91fc144f532033
Raviteja-SDET <ravitejayadav1998@gmail.com>
2025-01-03 16:53:33 +0530

    Initial commit

---
+++ src/com/java/testing/Main.java
@@ -0,0 +1,18 @@
+package com.java.testing;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Arun mootori- software testing - qafox ");
+        System.out.println("hello world");
+        System.out.println("100 days of java coding");
+        System.out.println("Welcome to java");
+        System.out.println(1+2);
+        System.out.println("1+2");
+        System.out.println("welcome"+123);
+
+
+    }
+}
---
+++ src/com/java/testing/VariablesDemo.java
@@ -0,0 +1,58 @@
+package com.java.testing;
+
+public class VariablesDemo {
+    public static void main(String[] args) {
+
+      //Example 1
+      /*  int a; //declaration
+        a = 100; //Assignment
+        System.out.println(a); //100
+        a = 200;
+        System.out.println(a); //200
+      */
+
+        //Example 2
+        /*
+        int a = 100; //declaration+assignment
+        System.out.println(a);
+        a = 200;
+        System.out.println(a);
+        */
+
+        //Example 3 - if all the variables are belong to same data type
+        /*
+        int a = 100;
+        int b = 200;
+        int c = 200;
+        */
+
+        //Example 4 - if all the variables are belong to same data type
+       /*
+       int a,b,c;
+       a = 100;
+       b = 200;
+       c = 300;
+       */
+
+       //Example 5 - in single statement we define multiple variables
+        int a = 100,b = 200,c = 300;
+
+        System.out.println("the value of a is "+a);
+        System.out.println("the value of b is "+b);
+        System.out.println("the value of c is "+c);
+
+        System.out.println(a+" "+b+" "+c);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
