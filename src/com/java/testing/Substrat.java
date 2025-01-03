fc558d9b162de110d5d454605eab99739cf6d886
Raviteja-SDET <ravitejayadav1998@gmail.com>
2025-01-03 18:54:45 +0530

    added substract.java

---
+++ src/com/java/testing/Substrat.java
@@ -0,0 +1,10 @@
+package com.java.testing;
+
+public class Substrat {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sub = a - b;
+        System.out.println(sub);
+    }
+}
